d5497733063a1caf309c0931517fd2295884c296
Shaoxi Liang <liangshaoxi@nudt.edu.cn>
2026-02-03 15:50:44 +0800

    batchsize=128

--- baseline.py
+++ baseline.py
@@ -776,7 +776,7 @@ def parse_args():
 
     # 训练参数
     parser.add_argument('--epochs', type=int, default=50, help='训练轮数')
-    parser.add_argument('--batch_size', type=int, default=64, help='批大小')
+    parser.add_argument('--batch_size', type=int, default=128, help='批大小')
     parser.add_argument('--lr', type=float, default=1e-4, help='初始学习率')
     parser.add_argument('--patience', type=int, default=10,
                         help='Early Stopping patience')
